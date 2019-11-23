33a90819e3dddc937841103b3a8025d39b7c5048
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2019-11-24 00:19:40 +0100

    rate limit only the POST requests

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 docker build -t my-nginx .
 docker run --rm --name some-nginx -d -p 8080:80 my-nginx
 siege --concurrent=10 --reps=10 --benchmark --no-parser 'http://localhost:8080'
+siege --concurrent=10 --reps=10 --benchmark --no-parser 'http://localhost:8080 POST coucou=1'
 docker kill some-nginx
